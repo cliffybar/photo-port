d9da669df949ba97bb2455195d3d0d83c5f8673e
cliffybar <crowellclifford@utexas.edu>
2021-06-24 16:58:13 -0500

    add photo gallery and contact form

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -1,3 +1,4 @@
+// __tests__/Nav.test.js with hard coded categories
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
@@ -9,8 +10,10 @@ describe('Nav component', () => {
     it('renders', () => {
         render(<Nav />);
     });
+
     it('matches snapshot', () => {
         const { asFragment } = render(<Nav />);
+
         expect(asFragment()).toMatchSnapshot();
     });
 })
@@ -18,6 +21,7 @@ describe('Nav component', () => {
 describe('emoji is visible', () => {
     it('inserts emoji into the h2', () => {
         const { getByLabelText } = render(<Nav />);
+
         expect(getByLabelText('camera')).toHaveTextContent('📸');
     });
 })
@@ -25,7 +29,9 @@ describe('emoji is visible', () => {
 describe('links are visible', () => {
     it('inserts text into the links', () => {
         const { getByTestId } = render(<Nav />);
+
         expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
         expect(getByTestId('about')).toHaveTextContent('About me');
     });
+
 })
